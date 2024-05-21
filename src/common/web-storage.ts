0acd7973eb4f9a37b955f64b80c4b34ef3251d96
荒 <1207588603@qq.com>
2024-05-21 15:07:29 +0800

    perf：二次封装 Storage 添加 keys 方法

--- src/common/web-storage.ts
+++ src/common/web-storage.ts
@@ -4,6 +4,7 @@ interface WebStorageConfig {
 }
 
 export class WebStorage {
+  /** 读取 */
   static getItem<T extends any>(key: string, config: Partial<WebStorageConfig> = {}): T | null {
     const storage = config.storage ?? window.localStorage
     const json = storage.getItem(key)
@@ -16,20 +17,30 @@ export class WebStorage {
     return value
   }
 
+  /** 存储 */
   static setItem(key: string, value: any, config: Partial<WebStorageConfig> = {}): void {
     const expired = config.expired ?? 0
+    if (isNaN(expired) || expired < 0) throw new Error(`expired must be a number`) // 过期时间值合理性判断
     const storage = config.storage ?? window.localStorage
     const data = { value, expired }
     storage.setItem(key, JSON.stringify(data))
   }
 
+  /** 删除 */
   static removeItem(key: string, config: Partial<WebStorageConfig> = {}) {
     const storage = config.storage ?? window.localStorage
     storage.removeItem(key)
   }
 
+  /** 清空 */
   static clear(config: Partial<WebStorageConfig> = {}) {
     const storage = config.storage ?? window.localStorage
     storage.clear()
+  }
+
+  /** 读取所有键 */
+  static keys(config: Partial<WebStorageConfig> = {}) {
+    const storage = config.storage ?? window.localStorage
+    return Object.keys(storage)
   }
 }
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,13 +1,5 @@
 <template>
-  <div class="app-container relative wh-full" :class="classes">
-    <el-card>
-      <el-table :data="tableData">
-        <el-table-column align="center" prop="date" label="日期" />
-        <el-table-column align="center" prop="name" label="姓名" />
-        <el-table-column align="center" prop="address" label="地址" />
-      </el-table>
-    </el-card>
-  </div>
+  <div class="app-container relative wh-full" :class="classes"></div>
 </template>
 
 <script setup lang="ts">
@@ -24,13 +16,6 @@ const classes = computed(() => {
   const cls: string[] = [appStore.device]
   return cls
 })
-
-const tableData = [
-  { date: '2016-05-03', name: 'Tom', address: 'No. 189, Grove St, Los Angeles' },
-  { date: '2016-05-02', name: 'Tom', address: 'No. 189, Grove St, Los Angeles' },
-  { date: '2016-05-04', name: 'Tom', address: 'No. 189, Grove St, Los Angeles' },
-  { date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles' },
-]
 </script>
 
 <style lang="scss" scoped>
